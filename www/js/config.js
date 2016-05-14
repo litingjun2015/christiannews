aaa1eb3af655d835b04bce934c7ffcf1c36d9512
litingjun <tingjun.li@gmail.com>
2016-05-14 12:38:03 +0800

    add news detail

---
+++ www/js/config.js
@@ -0,0 +1,19 @@
+angular.module('christiannews')
+ .constant('myConfig', (function () {
+   var backend = 'http://open.duanglink.com';
+   var backend_pay = 'http://www.duanglink.com/pay';
+   var backend_bizapp = 'http://www.duangwifi.cn/bizapp/v2-phone/www/';
+   //var backend_bizapp = 'http://192.168.31.193/bizapp/v2-phone/www/';
+   //var backend_bizapp = '';
+   var market = 'http://marketing.duanglink.com/backend/web';
+   return {
+
+    'backend_pay': backend_pay,
+
+     // bizapp / duangwifi
+     'client_id': 'EUyhtH5eKgXlJvAd-p3SH3yGwJHMGR2i',
+     'client_secret': 'xNOaiMYndFMmver8qkVzn5Wn2T9eZMEc',
+
+     'view_balanceonly': false,
+   }
+ })());
--- www/js/controllers/controllers.js
+++ www/js/controllers/controllers.js
@@ -3,15 +3,21 @@ angular.module('christiannews.controllers')
 .controller('HomeCtrl', function($scope, $rootScope, ToastService, $http) {
 
   $rootScope.getAlertStartId = 0;
+  $rootScope.fetchNum = 8;
   $rootScope.homenewslist = '';
 
+
+  $scope.$watch('$viewContentLoaded', function() {
+    $scope.doRefresh();
+
+  });
 
 
   $scope.doRefresh = function() {
 
     console.log('doRefresh');
 
-    var url="http://192.168.31.207:8081/listArticles/classid=1" + "&start="+ $rootScope.getAlertStartId + "&fetch=5";
+    var url="http://192.168.31.207:8081/listArticles/classid=1" + "&start="+ $rootScope.getAlertStartId + "&fetch=" + $rootScope.fetchNum;
     console.log(url);
     $http.get(url)
       .success(function (response)
@@ -23,12 +29,12 @@ angular.module('christiannews.controllers')
           $rootScope.homenewslist = response.concat($rootScope.homenewslist);
 
         console.log($rootScope.homenewslist);
-        $rootScope.getAlertStartId=$rootScope.getAlertStartId+5;
+        $rootScope.getAlertStartId=$rootScope.getAlertStartId+$rootScope.fetchNum;
 
       }).error(function(response) {
 
       ToastService.showShortCenter('获取数据失败');
-      $rootScope.getAlertStartId = $rootScope.getAlertStartId-10;
+      $rootScope.getAlertStartId = $rootScope.getAlertStartId-$rootScope.fetchNum;
 
     });
 
@@ -50,19 +56,57 @@ angular.module('christiannews.controllers')
 
 .controller('DashCtrl', function($scope) {})
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+.controller('ChatsCtrl', function($scope, $rootScope, ToastService, $http) {
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
+  $rootScope.getAlertStartId = 0;
+  $rootScope.fetchNum = 8;
+  $rootScope.homenewslist = '';
+
+
+  $scope.$watch('$viewContentLoaded', function() {
+    $scope.doRefresh();
+
+  });
+
+
+  $scope.doRefresh = function() {
+
+    console.log('doRefresh');
+
+    var url="http://192.168.31.207:8081/listArticles/classid=1" + "&start="+ $rootScope.getAlertStartId + "&fetch=" + $rootScope.fetchNum;
+    console.log(url);
+    $http.get(url)
+      .success(function (response)
+      {
+
+        if($rootScope.homenewslist == '')
+          $rootScope.homenewslist = response;
+        else
+          $rootScope.homenewslist = response.concat($rootScope.homenewslist);
+
+        console.log($rootScope.homenewslist);
+        $rootScope.getAlertStartId=$rootScope.getAlertStartId+$rootScope.fetchNum;
+
+      }).error(function(response) {
+
+      ToastService.showShortCenter('获取数据失败');
+      $rootScope.getAlertStartId = $rootScope.getAlertStartId-$rootScope.fetchNum;
+
+    });
+
+
+    //    $http.get('/new-items')
+    //     .success(function(newItems) {
+    //       $scope.items = newItems;
+    //     })
+    //     .finally(function() {
+    //       // Stop the ion-refresher from spinning
+    //       $scope.$broadcast('scroll.refreshComplete');
+    //     });
+
+    $scope.$broadcast('scroll.refreshComplete');
   };
+
 })
 
 .controller('ChatDetailCtrl', function($scope, $stateParams, Chats, $http) {
